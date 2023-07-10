c359fa6fed343b386356e7be088f178459af0989
Richard Weeks <richard.weeks1@digital.homeoffice.gov.uk>
2023-07-10 17:33:38 +0100

    Add message with timestamp.

--- app/src/main/java/com/example/textdemo/MainActivity.kt
+++ app/src/main/java/com/example/textdemo/MainActivity.kt
@@ -1,26 +1,35 @@
 package com.example.textdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.example.textdemo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        lateinit var viewModel : TextViewModel
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding : ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
         val enterButton: Button = findViewById(R.id.button)
-        val resultTextView: TextView = findViewById(R.id.textView)
+        //binding.button.setOnClickListener()
+        val resultTextView: ListView = findViewById(R.id.listview)
         val inputText: EditText = findViewById(R.id.editText)
-        viewModel = ViewModelProvider(this).get(TextViewModel::class.java)
+        var viewModel : TextViewModel =
+            ViewModelProvider(this).get(TextViewModel::class.java)
+        val itemsAdapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, viewModel.getInputText())
+        resultTextView.adapter = itemsAdapter
         enterButton.setOnClickListener {
             viewModel.addInputText(inputText.text.toString())
-            resultTextView.text = inputText.text
+            itemsAdapter.notifyDataSetChanged()
         }
     }
 }
--- app/src/main/java/com/example/textdemo/TextViewModel.kt
+++ app/src/main/java/com/example/textdemo/TextViewModel.kt
@@ -1,17 +1,33 @@
 package com.example.textdemo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 class TextViewModel : ViewModel() {
 
+    private val liveText : MutableLiveData<List<String>> = MutableLiveData()
+
     private val inputText = arrayListOf<String>()
 
+
     fun addInputText(text : String) {
-        inputText.add(text)
+        val timeStamp: String = SimpleDateFormat("HH:mm").format(Date())
+        val entry : String = "[$timeStamp] $text"
+        inputText.add(entry)
     }
 
     fun getInputText(): ArrayList<String> {
         return inputText
+    }
+
+    fun addTextToList(str: String) {
+        inputText.add(str)
+        liveText.value = inputText
     }
 
     override fun onCleared() {
