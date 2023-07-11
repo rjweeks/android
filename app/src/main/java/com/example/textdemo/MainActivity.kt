c24f8a961446f1fa076f85affa1c5d308680da9f
Richard Weeks <richard.weeks1@digital.homeoffice.gov.uk>
2023-07-11 17:17:44 +0100

    Use recycler view.

--- app/src/main/java/com/example/textdemo/MainActivity.kt
+++ app/src/main/java/com/example/textdemo/MainActivity.kt
@@ -1,40 +1,47 @@
 package com.example.textdemo
 
-import android.app.Activity
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
-import android.view.inputmethod.InputMethodManager
-import android.widget.AbsListView
+import android.view.View
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.textdemo.databinding.ActivityMainBinding
 import java.io.File
 import java.io.OutputStreamWriter
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var textAdapter: TextAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding : ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+            DataBindingUtil.setContentView(this, com.example.textdemo.R.layout.activity_main)
         val viewModel : TextViewModel =
             ViewModelProvider(this).get(TextViewModel::class.java)
-        val itemsAdapter: ArrayAdapter<String> =
-            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, viewModel.getInputText())
-        binding.listview.adapter = itemsAdapter
+        val recyclerView: RecyclerView = findViewById<View>(com.example.textdemo.R.id.recyclerView) as RecyclerView
+//        val itemsAdapter: ArrayAdapter<String> =
+//            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, viewModel.getInputText())
+        textAdapter = TextAdapter(viewModel.getInputText())
+        ViewCompat.setNestedScrollingEnabled(recyclerView, false);
+        val layoutManager = LinearLayoutManager(applicationContext)
+//        binding.recyclerView.adapter = itemsAdapter
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = textAdapter
         binding.lifecycleOwner = this
         binding.viewmodel = viewModel
         binding.button.setOnClickListener {
             val text = binding.editText.text.toString()
             viewModel.addInputText(text)
-            itemsAdapter.notifyDataSetChanged()
-            writeFile(text)
+            textAdapter.notifyDataSetChanged()
+            //writeFile(text)
         }
-
     }
 
 
